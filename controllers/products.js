e119e8538a8e6a99d658c3cef1cae89ea2d7205d
phsu40 <phsu40@gatech.edu>
2023-06-04 15:24:05 -0700

    Output product data into products.json

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -17,13 +17,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getAllProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = []
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../util/path");
+const filePath = path.join(rootDir, "data", "products.json");
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +10,25 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    
+    fs.readFile(filePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this)
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    fs.readFile(filePath, (err, fileContent) => {
+      if (err || !fileContent) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    })
   }
-}
+};
