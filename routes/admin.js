f1baba0afa25fa396056b8a6d3107cd70271c6d4
phsu40 <phsu40@gatech.edu>
2023-06-05 09:33:31 -0700

    Add additional routes & controllers for new navigation endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
 
+router.get('/products')
+
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
+
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
+
+router.get("/products", productsController.getAllProducts);
+
+router.get('/cart');
+
+router.get('/checkout');
 
 module.exports = router;
