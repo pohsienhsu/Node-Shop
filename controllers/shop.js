4ad3c4c1cfffe7bf38282ff89c2e32a33ade75ee
phsu40 <phsu40@gatech.edu>
2023-06-06 10:27:27 -0700

    Integrate dynamic route path for getting product details based on their ID

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,14 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    console.log(product);
+  })
+  res.redirect('/')
+} 
+
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
