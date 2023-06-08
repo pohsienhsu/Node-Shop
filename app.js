974a0d2ef94378f575c98852877b121d1c1d746b
phsu40 <phsu40@gatech.edu>
2023-06-08 14:25:44 -0700

    Impelement read product data from SQL database

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
 
 const app = express();
 
@@ -14,14 +13,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-db.execute('SELECT * FROM products')
-  .then((result) => {
-    console.log(result[0]);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- models/product.js
+++ models/product.js
@@ -1,20 +1,6 @@
-const fs = require("fs");
-const path = require("path");
+const db = require('../util/database');
 
 const Cart = require('./cart');
-
-const getShopProductsFromFile = (cb) => {
-  fs.readFile(filePath, (err, fileContent) => {
-    if (err || !fileContent) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-const rootDir = require("../util/path");
-const filePath = path.join(rootDir, "data", "products.json");
 
 module.exports = class Product {
   constructor(id, title, imageUrl, price, description) {
@@ -26,54 +12,22 @@ module.exports = class Product {
   }
 
   save() {
-    if (this.id) {
-      getShopProductsFromFile((products) => {
-        const existingProductIndex = products.findIndex(
-          (p) => p.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        Cart.editProduct(this.id, this.price);
-        fs.writeFile(filePath, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
-        });
-      });
-    } else {
-      this.id = Math.random().toString();
-      getShopProductsFromFile((products) => {
-        products.push(this);
-        fs.writeFile(filePath, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      });
-    }
+    return db.execute('INSERT INTO products(title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.imageUrl, this.price, this.description]
+    );
   }
 
   static deleteById(id) {
-    getShopProductsFromFile((products) => {
-      const existingProductIndex = products.findIndex((p) => p.id === id);
-      const price = products[existingProductIndex].price;
-      if (existingProductIndex === -1) {
-        console.log("Delete Failed. This product doesn't exist: " + id);
-        return;
-      }
-      const updatedProducts = products.filter((p) => p.id !== id);
-      fs.writeFile(filePath, JSON.stringify(updatedProducts), (err) => {
-        if (!err) {
-          Cart.deleteProduct(id, price, () => {});
-        }
-      });
-    });
+
   }
 
-  static fetchAll(cb) {
-    getShopProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
   }
 
-  static findById(id, cb) {
-    getShopProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
-      cb(product);
-    });
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', 
+     [id]
+    )
   }
 };
