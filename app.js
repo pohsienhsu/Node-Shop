8ae223af0805b93e768c9317140f08bb1de395e6
phsu40 <phsu40@gatech.edu>
2023-06-08 13:40:04 -0700

    Integrate MySQL database with app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require('path');
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -12,6 +14,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
