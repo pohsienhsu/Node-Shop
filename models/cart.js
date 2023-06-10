ff4cbef8db3147bddacdbc284b4ba5723876f811
phsu40 <phsu40@gatech.edu>
2023-06-10 16:51:54 -0700

    Integrate associations between User and Produt along with updated controller methods

--- models/cart.js
+++ models/cart.js
@@ -1,25 +1,14 @@
-const db = require("../util/database");
+const Sequelize = require('sequelize');
 
-module.exports = class Cart {
-  constructor() {
-    this.products = [];
-    this.totalPrice = 0.0;
-  }
+const sequelize = require('../util/database');
 
-  static getCart() {
-    return db.execute(
-      "SELECT products.id, products.title, products.price, cart.quantity FROM products " +
-        "INNER JOIN " +
-        "(SELECT product_id, COUNT(cart_items.id) AS quantity FROM cart_items GROUP BY cart_items.product_id) AS cart " +
-        "ON cart.product_id = products.id;"
-    );
-  }
+const Cart = sequelize.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+});
 
-  static addProduct(id) {
-    return db.execute("INSERT INTO cart_items (product_id) VALUES (?);", [id]);
-  }
-
-  static deleteProduct(id) {
-    return db.execute('DELETE FROM cart_items WHERE product_id=?', [id]);
-  }
-};
+module.exports = Cart;
