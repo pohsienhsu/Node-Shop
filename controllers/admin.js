77291c2e6ffe98d75281511d931bde3e8755f832
phsu40 <phsu40@gatech.edu>
2023-06-12 13:47:23 -0700

    Integrate MongoDB to edit existing products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb");
+
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -20,41 +22,38 @@ exports.postAddProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getProducts = (req, res, next) => {
-//   req.user
-//     .getProducts()
-//     .then((products) => {
-//       res.render("admin/products", {
-//         prods: products,
-//         pageTitle: "Admin Products",
-//         path: "/admin/products",
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit === "true";
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
-//   const prodId = req.params.productId;
-//   req.user
-//     .getProducts({ where: { id: prodId } })
-//     .then((products) => {
-//       const product = products[0];
-//       if (!product) {
-//         return res.redirect("/admin/products");
-//       }
-//       res.render("admin/edit-product", {
-//         pageTitle: `Edit ${product.title}`,
-//         path: "/admin/edit-product",
-//         editing: editMode,
-//         product: product,
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit === "true";
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findOne(new ObjectId(prodId))
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/admin/products");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: `Edit ${product.title}`,
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 // exports.postEditProduct = (req, res, next) => {
 //   const { id, title, imageUrl, price, description } = req.body;
--- routes/admin.js
+++ routes/admin.js
@@ -12,11 +12,11 @@ router.get("/add-product", adminController.getAddProduct);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
-// // /admin/products => GET
-// router.get("/products", adminController.getProducts);
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
-// // /admin/edit-product/:productId => GET
-// router.get("/edit-product/:productId", adminController.getEditProduct);
+// /admin/edit-product/:productId => GET
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 // // /admin/edit-product => POST
 // router.post("/edit-product", adminController.postEditProduct);
