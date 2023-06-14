93e71e796be15b7efb54389e5ad02e1f34025f6b
phsu40 <phsu40@gatech.edu>
2023-06-14 10:17:48 -0700

    Integrate all cart functionality: add product, delete product with MongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,13 +40,10 @@ exports.getProduct = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart.getProducts();
-    })
     .then((products) => {
       let totalPrice = 0;
       for (let product of products) {
-        totalPrice = totalPrice + product.price * product.cartItem.quantity;
+        totalPrice = totalPrice + product.price * product.quantity;
       }
       res.render("shop/cart", {
         path: "/cart",
@@ -60,33 +57,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      let product;
-      if (products.length > 0) {
-        product = products[0];
-      }
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-        return product;
-      }
-      return Product.findByPk(prodId);
-    })
+  Product.findById(prodId)
     .then((product) => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity },
-      });
+      return req.user.addToCart(product);
     })
     .then((result) => {
-      return res.redirect("/cart");
+      res.redirect("/cart");
     })
     .catch((err) => console.log(err));
 };
@@ -94,14 +70,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const { id: prodId } = req.body;
   req.user
-    .getCart()
-    .then((cart) => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .removeFromCart(prodId)
     .then((result) => {
       console.log("Cart Item Deleted");
       res.redirect("/cart");
