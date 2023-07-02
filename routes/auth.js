90fd76fa584162a84ad09e49f9e6523e4bc157b9
phsu40 <phsu40@gatech.edu>
2023-07-01 17:30:42 -0700

    Create GET route for new password submission form & POST route for new password submission

--- routes/auth.js
+++ routes/auth.js
@@ -17,5 +17,11 @@ router.post('/signup', authController.postSignup);
 
 router.get('/reset', authController.getReset);
 
+router.post('/reset', authController.postReset);
+
+router.get('/new-password/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
 module.exports = router;
 
