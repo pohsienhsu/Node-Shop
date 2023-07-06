b42a013044d94f487b1602f44e178ea29856f46c
phsu40 <phsu40@gatech.edu>
2023-07-06 13:39:49 -0700

    Refactor POST error handling

--- app.js
+++ app.js
@@ -49,6 +49,11 @@ app.use(
 );
 
 app.use(csrfSynchronisedProtection);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn ? true : false;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use((req, res, next) => {
   if (req.session.user) {
@@ -61,28 +66,26 @@ app.use((req, res, next) => {
         next();
       })
       .catch((err) => {
-        throw new Error(err);
+        next(new Error(err));
       });
   } else {
     next();
   }
 });
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn ? true : false;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect('/500')
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  })
+  res.redirect('/500');
 })
 
 mongoose
